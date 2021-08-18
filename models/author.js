f40824cea6d5b578d918e46e555bbfed35f5fc99
Tamara Trefilova <tamara.trefilova@gmail.com>
2021-08-17 21:40:40 -0700

    added date2

--- models/author.js
+++ models/author.js
@@ -4,7 +4,8 @@ const authorSchema = new mongoose.Schema({
     name: { type: String, required: true },
     gender: { type: String },
     yearBorn: { type: Number },
-    date: { type: Number }
+    date: { type: Number },
+    date2: { type: Number }
 });
 
 
