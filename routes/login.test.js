53a58e59b514c225cda6ca2c282c3b25b6fd0637
Surry <50641921+surryjm@users.noreply.github.com>
2021-08-20 21:58:19 -0700

    added first login test and test data

--- routes/login.test.js
+++ routes/login.test.js
@@ -4,3 +4,30 @@ const server = require('../server');
 const testUtils = require('../test-utils');
 const User = require('../models/user');
 
+describe('/login', () => {
+  beforeAll(testUtils.connectDB);
+  afterAll(testUtils.stopDB);
+  afterEach(testUtils.clearDB);
+
+  const user0 = {
+    email: 'user0@mail.com',
+    password: '123password'
+  };
+  const user1 = {
+    email: 'user1@mail.com',
+    password: '456password'
+  }
+
+  describe('before signup', () => {
+    describe('POST /', () => {
+      it('should return 401', async () => {
+        const res = await (await request(server).post('/login')).setEncoding(user0);
+        expect(res.statusCode).toEqual(401);
+      });
+    });
+    
+
+  });
+
+
+});
