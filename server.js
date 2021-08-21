d1b28ebba506169c02b5bf151568937daa8a29ed
Surry <50641921+surryjm@users.noreply.github.com>
2021-08-21 16:59:46 -0700

    added globalConfig to gitignore, added mustache reference to server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+//const mustacheExpress = require('mustache-express');
 
 const routes = require("./routes");
 
 const server = express();
+//server.engine('mustache', mustacheExpress());
+//server.set('view engine', 'mustache');
+
 server.use(express.json());
 
 server.use(routes);
