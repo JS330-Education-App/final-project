649cef825b454b46e140fb642f106d42664ee21f
Patrick Gronstal <gronstal@uw.edu>
2021-08-21 21:37:42 -0600

    Updated files for Hosting.  Added dotenv npm module.  Added .env file.  Modified index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require("./server");
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost/final-project', {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
