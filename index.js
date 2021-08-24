2afaa40663349936cd7012297956a4e4c68327ce
Patrick Gronstal <gronstal@uw.edu>
2021-08-24 03:52:24 -0600

    Fixed index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb+srv://patrick:uBlCC74eKHTv8Ahg@education-app.cidu4.mongodb.net/education-app?retryWrites=true&w=majority', {
+mongoose.connect('process.env.MONGO_CONNECTION_STRING', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
