783b53c9b75593f789377ba34ad8de91fc11848c
kattiperk <kattiperk@gmail.com>
2021-08-26 22:46:35 -0700

    UI improved, templates added for repeating HTML parts

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,19 +1,19 @@
 const role = document.getElementById('role-select');
 
 role.addEventListener('change', function() {
-    if (role.value === 'teacher' || role.value === 'student' || role.value === 'parent') {
+    if (role.value === 'teachers' || role.value === 'students' || role.value === 'parents') {
         document.getElementById('form-common').classList.remove('invisible');
     } else {
         document.getElementById('form-common').classList.add('invisible');
     }
 
-    if (role.value === 'student') {
+    if (role.value === 'students') {
         document.getElementById('form-student').classList.remove('invisible');
     } else {
         document.getElementById('form-student').classList.add('invisible');
     }
 
-    if (role.value === 'parent') {
+    if (role.value === 'parents') {
         document.getElementById('form-parent').classList.remove('invisible');
     } else {
         document.getElementById('form-parent').classList.add('invisible');
